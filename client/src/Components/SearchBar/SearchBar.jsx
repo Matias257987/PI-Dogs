3ed0cfa64392b1ce0fffde268d1bf0f62efcfd67
Matias <acostamatias25@gmail.com>
2023-02-15 15:04:30 -0300

    deploy

--- client/src/Components/SearchBar/SearchBar.jsx
+++ client/src/Components/SearchBar/SearchBar.jsx
@@ -4,24 +4,34 @@ import { getBreed } from "../../Redux/actions/index";
 import style from "./SearchBar.module.css";
 
 export default function SearchBar() {
-    const dispatch = useDispatch();
-    const [searchDog, setSearchDog] = useState("");
+  const dispatch = useDispatch();
+  const [searchDog, setSearchDog] = useState("");
 
-    const handleInput = (e) => {
-        e.preventDefault();
-        setSearchDog(e.target.value);
-    };
+  const handleInput = (e) => {
+    e.preventDefault();
+    setSearchDog(e.target.value);
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        dispatch(getBreed(searchDog));
-    };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(getBreed(searchDog));
+  };
 
-    return (
-        <div className={`${style.searchBar_container}`}>
-            <input className={`${style.searchBar}`} type="text" onChange={handleInput} placeholder='Search...' />
-            <button className={`${style.searchBar_button}`} type="submit" onClick={handleSubmit}>Submit
-            </button>
-        </div>
-    )
+  return (
+    <div className={`${style.searchBar_container}`}>
+      <input
+        className={`${style.searchBar}`}
+        type="text"
+        onChange={handleInput}
+        placeholder="Search..."
+      />
+      <button
+        className={`${style.searchBar_button}`}
+        type="submit"
+        onClick={handleSubmit}
+      >
+        Submit
+      </button>
+    </div>
+  );
 }
